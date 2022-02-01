32a547b93973884b661bef66a02a7777ca885082
GitHub Action <action@github.com>
2022-02-01 09:37:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/gingerboy14/componentfoldercreator/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/gingerboy14/componentfoldercreator/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.gingerboy14.componentfoldercreator.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.gingerboy14.componentfoldercreator.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/gingerboy14/componentfoldercreator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.gingerboy14.componentfoldercreator.services
+
+import com.github.gingerboy14.componentfoldercreator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
